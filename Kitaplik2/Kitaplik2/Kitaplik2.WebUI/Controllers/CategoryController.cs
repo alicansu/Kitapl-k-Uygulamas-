a2a7e430d9cc6b1e144692a84c83a5c64cade08c
alicansu <51889299+alicansu@users.noreply.github.com>
2025-11-22 22:48:13 +0300

    Create CategoryController.cs

---
+++ Kitaplik2/Kitaplik2/Kitaplik2.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kitaplik2.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
